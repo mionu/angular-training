bebe5e9f95558d327994b1242b6724361d1bf5ae
Yuliya Karenik <Yuliya_Karenik@epam.com>
2018-06-22 16:37:37 +0300

    task 1: update date format

--- src/app/courses-list/courses.service.ts
+++ src/app/courses-list/courses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Course } from './course.model';
-import mockData from './mock-data';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,19 @@ export class CoursesService {
   constructor() { }
 
   public getCoursesList(): Course[] {
-    return mockData;
+    const dateFormat: string = 'DD MMM YYYY';
+    return [{
+      'id': 1,
+      'title': 'course 1',
+      'creationDate': moment('2017-03-15').format(dateFormat),
+      'duration': 60,
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }, {
+      'id': 2,
+      'title': 'course 2',
+      'creationDate': moment('2018-06-01').format(dateFormat),
+      'duration': 80,
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }];
   }
 }
