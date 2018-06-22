6b9f93896d857930e6804c8ad0f34e21a24b0c6c
mionu <juliekorenik@gmail.com>
2018-06-22 18:26:17 +0300

    task 1: fix footer & date

--- src/app/courses-list/courses.service.ts
+++ src/app/courses-list/courses.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Course } from './course.model';
-import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +9,23 @@ export class CoursesService {
   constructor() { }
 
   public getCoursesList(): Course[] {
-    const dateFormat: string = 'DD MMM YYYY';
     return [{
       'id': 1,
       'title': 'course 1',
-      'creationDate': moment('2017-03-15').format(dateFormat),
+      'creationDate': '15 Mar 2017',
       'duration': 60,
       'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }, {
       'id': 2,
       'title': 'course 2',
-      'creationDate': moment('2018-06-01').format(dateFormat),
+      'creationDate': '01 Jun 2018',
       'duration': 80,
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }, {
+      'id': 3,
+      'title': 'course 3',
+      'creationDate': '11 Jan 2018',
+      'duration': 95,
       'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }];
   }
