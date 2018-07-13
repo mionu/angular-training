c1776a37f47c615d1a18c51b95eea8bca9b5b343
Yuliya Karenik <Yuliya_Karenik@epam.com>
2018-07-13 13:03:51 +0300

    task 4: duration pipe

--- src/app/courses-list/course-duration.pipe.ts
+++ src/app/courses-list/course-duration.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CourseDurationPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: number): string {
+    const hours = Math.floor(value / 60);
+    const minutes = value % 60;
+    return `${hours > 0 ? hours + 'h' : ''} ${minutes > 0 ? minutes + 'min' : ''}`;
   }
 
 }
--- src/app/courses-list/courses.service.ts
+++ src/app/courses-list/courses.service.ts
@@ -27,7 +27,7 @@ export class CoursesService {
       id: 3,
       title: 'course 3',
       creationDate: moment('05-11-2018').toDate(),
-      duration: 95,
+      duration: 45,
       topRated: true,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }];
