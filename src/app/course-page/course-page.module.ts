8391a8f563cf66748d36d540a41c0d30bf07be98
mionu <juliekorenik@gmail.com>
2018-07-28 23:58:50 +0300

    task 7: auth guard

--- src/app/course-page/course-page.module.ts
+++ src/app/course-page/course-page.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CoursePageComponent } from './course-page/course-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { CoursePageComponent } from './course-page/course-page.component';
 
 @NgModule({
   imports: [
